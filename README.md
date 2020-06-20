18aa951ae21cca155763b6203e500c000345e42f
bloski <michael@blo.ski>
2020-06-20 22:13:42 +0200

    update discord_example.php

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # ZAJEC discord stats
+
+## Installation
+1. `composer install`
+2. `cp config/discord_example.php config/discord.php`
+
+NOTE: muffin/oauth2 is broken. Replace `$event->result` with `$event->getResult()` in `OAuthAuthenticate.php`.
--- config/discord.example.php
+++ config/discord.example.php
@@ -1,6 +1,13 @@
 <?php
 return [
     'discord' => [
+        'oauth' => [
+            'url' => 'https://discord.com/api/oauth2/authorize?BLAH',
+            'clientId' => 'CLIENT_ID',
+            'clientSecret' => 'CLIENT_SECRET',
+            'redirectUri' => 'http://zajec.localhost/oauth/discord',
+        ],
+
         'token' => 'DISCORD_TOKEN',
         'guild' => 'GUILD_ID',
     ],
