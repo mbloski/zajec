9c32503f8118d177bc8aa0b705117d12965bc5df
bloski <michael@blo.ski>
2020-06-27 14:06:03 +0200

    set column widths in top channels

--- templates/Stats/index.php
+++ templates/Stats/index.php
@@ -250,15 +250,15 @@ use Cake\Utility\Hash;
         <?php if (!$key) continue; ?>
             <tr class="itemrow">
                 <td class="itemdesc">#<?= $n + 1 ?></td>
-                <td class="itemdesc bold"><?= $this->Html->link('#'.$guildChannels[$key]['name'], ['controller' => 'Logs', 'action' => 'index', '?' => ['channel' => $guildChannels[$key]['id']]]) ?></td>
+                <td class="itemdesc bold" style="min-width:180px;"><?= $this->Html->link('#'.$guildChannels[$key]['name'], ['controller' => 'Logs', 'action' => 'index', '?' => ['channel' => $guildChannels[$key]['id']]]) ?></td>
                 <td class="itemdesc"><?= $channel->count ?></td>
-                <td class="itemdesc"><?= $this->Html->link($this->Discord->getUsernameWithColor($channel->most_active) ?? $channel->most_active, ['controller' => 'Stats', 'action' => 'user', $channel->most_active], ['escape' => false]) ?></td>
+                <td class="itemdesc" style="min-width:180px;word-break:break-word;"><?= $this->Html->link($this->Discord->getUsernameWithColor($channel->most_active) ?? $channel->most_active, ['controller' => 'Stats', 'action' => 'user', $channel->most_active], ['escape' => false]) ?></td>
                 <?php
                     $user = substr($channel->random_message, 0, strpos($channel->random_message, '>>'));
                     $line = substr($channel->random_message, strpos($channel->random_message, $user) + strlen($user) + 3);
                     $user = str_replace(['<', '>', '@', '!'], '', $user);
                 ?>
-                <td class="itemdesc">
+                <td class="itemdesc" style="max-width:600px;word-break:break-word;">
                     <div class="rich-line">
                         <?= '&lt;'.$this->Html->link($this->Discord->getUsernameWithColor($user), ['controller' => 'Stats', 'action' => 'user', $user], ['escape' => false]).'&gt; ' ?> <?= $this->Log->richLine($line) ?>
                     </div>
